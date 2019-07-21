fb979db6698383ed6de266a68ec74edf9681fc6e
Adam Milne-Smith <adam@milne-smith.co.uk>
2019-07-21 13:30:38 +0100

    Adds basic ppm image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let nx = 200u32;
+    let ny = 100u32;
+    println!("P3");
+    println!("{} {}", nx, ny);
+    println!("255");
+    for j in 0..ny {
+        for i in 0..nx {
+            let r = i as f32 / nx as f32;
+            let g = j as f32 / ny as f32;
+            let b = 0.2f32;
+            let ir = (255.99*r) as u32;
+            let ig = (255.99*g) as u32;
+            let ib = (255.99*b) as u32;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
