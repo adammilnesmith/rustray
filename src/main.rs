7aae03eda0a89e1db61bbfef2eff250244523d0a
Adam Milne-Smith <adam@milne-smith.co.uk>
2019-08-06 08:01:00 +0100

    Use f64::from instead of as in main

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,11 @@ fn main() {
     println!("255");
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let vector: Vec3<f64> =
-                Vec3::new(i as f64 / nx as f64, j as f64 / ny as f64, 0.2f64) * 255.99;
+            let vector: Vec3<f64> = Vec3::new(
+                f64::from(i) / f64::from(nx),
+                f64::from(j) / f64::from(ny),
+                0.2f64,
+            ) * 255.99;
             println!(
                 "{} {} {}",
                 vector.x as u32, vector.y as u32, vector.z as u32
