f16286493a8e4fed663e7467fc46b836e9962370
Adam Milne-Smith <adam@milne-smith.co.uk>
2019-08-08 08:09:37 +0100

    Use Option for hit

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn color(ray: Ray<f64>) -> Vec3<f64> {
     let sphere_center = Vec3::new(0.0, 0.0, -1.0);
     let sphere_radius = 0.5;
     let t = hit_sphere(ray, sphere_center, sphere_radius);
-    if t > 0.0 {
-        let hit_point = ray.point_at_parameter(t);
+    if t.is_some() {
+        let hit_point = ray.point_at_parameter(t.unwrap());
         let normal = (hit_point - sphere_center).unit();
         sphere_radius * normal.map(|i: f64| -> f64 { i + 1.0 })
     } else {
@@ -16,16 +16,16 @@ fn color(ray: Ray<f64>) -> Vec3<f64> {
     }
 }
 
-fn hit_sphere(ray: Ray<f64>, center: Vec3<f64>, radius: f64) -> f64 {
+fn hit_sphere(ray: Ray<f64>, center: Vec3<f64>, radius: f64) -> Option<f64> {
     let oc = ray.origin() - center;
     let a = ray.direction().dot(ray.direction());
     let b = 2.0 * oc.dot(ray.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
     if discriminant < 0.0 {
-        -1.0 // TODO: make this an Option
+        None
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
     }
 }
 
