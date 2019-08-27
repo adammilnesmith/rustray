63bb883ff840062a1a06b4aa723a7acf5e296ac0
Adam Milne-Smith <adam@milne-smith.co.uk>
2019-08-27 20:02:23 +0100

    Adds percentage completion to window. Renames Canvas to ImageData now that it has a completion field.

--- src/image_data.rs
+++ src/image_data.rs
@@ -8,9 +8,10 @@ macro_rules! vec_no_clone {
     }};
 }
 
-pub struct Canvas<T> {
+pub struct ImageData<T> {
     pub x_size: usize,
     pub y_size: usize,
+    pub complete: RwLock<f64>,
     pub canvas: Vec<RwLock<T>>, //TODO: modify this to use AtomicPtr later
 }
 
@@ -19,24 +20,31 @@ fn to_index(x: usize, y: usize, x_size: usize) -> usize {
     y * x_size + x
 }
 
-impl<T> Canvas<T>
+impl<T> ImageData<T>
 where
     T: Clone,
 {
     #[inline]
-    fn new(x_size: usize, y_size: usize, canvas: Vec<RwLock<T>>) -> Canvas<T> {
-        Canvas {
+    fn new(
+        x_size: usize,
+        y_size: usize,
+        complete: RwLock<f64>,
+        canvas: Vec<RwLock<T>>,
+    ) -> ImageData<T> {
+        ImageData {
             x_size,
             y_size,
+            complete,
             canvas,
         }
     }
 
     #[inline]
-    pub fn new_blank(x_size: usize, y_size: usize, init_value: T) -> Canvas<T> {
-        Canvas::new(
+    pub fn new_blank(x_size: usize, y_size: usize, init_value: T) -> ImageData<T> {
+        ImageData::new(
             x_size,
             y_size,
+            RwLock::new(0.0),
             vec_no_clone![RwLock::new(init_value.clone()); x_size * y_size],
         )
     }
@@ -49,7 +57,20 @@ where
         self.y_size
     }
 
-    pub fn read_pixel(&self, x: usize, y: usize) -> T {
+    pub fn get_complete(&self) -> f64 {
+        self.complete.read().unwrap().clone()
+    }
+
+    pub fn update_complete<F>(&self, update: F) -> f64
+    where
+        F: Fn(f64) -> f64,
+    {
+        let mut value = self.complete.write().unwrap();
+        *value = update((*value).clone());
+        (*value).clone()
+    }
+
+    pub fn get_pixel(&self, x: usize, y: usize) -> T {
         self.canvas
             .get(to_index(x, y, self.x_size))
             .unwrap()
@@ -73,7 +94,7 @@ where
     }
 }
 
-impl<T> Into<Vec<T>> for Canvas<T>
+impl<T> Into<Vec<T>> for ImageData<T>
 where
     T: Clone,
 {
@@ -85,7 +106,7 @@ where
     }
 }
 
-impl<T> Into<Vec<T>> for &Canvas<T>
+impl<T> Into<Vec<T>> for &ImageData<T>
 where
     T: Clone,
 {
@@ -103,12 +124,12 @@ mod tests {
 
     #[test]
     fn test_blank_canvas() {
-        assert_eq!(Canvas::new_blank(16, 9, 0.0).canvas.len(), 16 * 9);
+        assert_eq!(ImageData::new_blank(16, 9, 0.0).canvas.len(), 16 * 9);
     }
 
     #[test]
     fn test_multi_threaded_access() {
-        let canvas = Arc::new(Canvas::new_blank(1, 1, 0.0));
+        let canvas = Arc::new(ImageData::new_blank(1, 1, 0.0));
 
         let mut handles = vec![];
 
@@ -129,17 +150,17 @@ mod tests {
         .map(|handle| handle.join().unwrap());
         */
 
-        assert_eq!(canvas.read_pixel(0, 0), 10.0);
+        assert_eq!(canvas.get_pixel(0, 0), 10.0);
     }
 
     #[test]
     fn test_into_vec() {
-        let canvas = Canvas::new_blank(2, 1, 0.0);
+        let canvas = ImageData::new_blank(2, 1, 0.0);
         let as_vec: Vec<f64> = (&canvas).into();
         assert_eq!(as_vec, vec![0.0, 0.0]);
         canvas.update_pixel(0, 0, |_| 1.0);
-        assert_eq!(canvas.read_pixel(0, 0), 1.0);
-        assert_eq!(canvas.read_pixel(1, 0), 0.0);
+        assert_eq!(canvas.get_pixel(0, 0), 1.0);
+        assert_eq!(canvas.get_pixel(1, 0), 0.0);
         assert_eq!(as_vec, vec![0.0, 0.0]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate image;
+
+extern crate find_folder;
 extern crate piston_window;
 extern crate rand;
 
@@ -14,15 +16,15 @@ use rand::prelude::ThreadRng;
 use rand::Rng;
 
 use camera::Camera;
-use canvas::Canvas;
 use hittable::{Hittable, Sphere, World};
+use image_data::ImageData;
 use material::{LightInteraction, Material};
 use ray::Ray;
 use vec3::Vec3;
 
 mod camera;
-mod canvas;
 mod hittable;
+mod image_data;
 mod material;
 mod ray;
 mod vec3;
@@ -86,9 +88,9 @@ fn main() {
     let nx = 1280usize;
     let ny = 720usize;
 
-    let canvas = Arc::new(Canvas::new_blank(nx, ny, Vec3::new(0.0, 0.0, 0.0)));
+    let image_data = Arc::new(ImageData::new_blank(nx, ny, Vec3::new(0.0, 0.0, 0.0)));
 
-    let window_thread = run_window_thread(Arc::clone(&canvas));
+    let window_thread = run_window_thread(Arc::clone(&image_data));
 
     let camera: Camera<f64> = Camera::new(
         Vec3::new(0.0, 0.0, 0.0),
@@ -100,12 +102,12 @@ fn main() {
     let world = create_world();
 
     let samples = 9;
-    draw_on_canvas(&canvas, &camera, &world, samples);
-    output_ppm(canvas);
+    draw_to_image_data(&image_data, &camera, &world, samples);
+    output_ppm(image_data);
     //window_thread.join().unwrap();
 }
 
-fn run_window_thread(image_data: Arc<Canvas<Vec3<f64>>>) -> JoinHandle<()> {
+fn run_window_thread(image_data: Arc<ImageData<Vec3<f64>>>) -> JoinHandle<()> {
     thread::spawn(move || {
         let width = image_data.x_size() as u32;
         let height = image_data.y_size() as u32;
@@ -119,6 +121,13 @@ fn run_window_thread(image_data: Arc<Canvas<Vec3<f64>>>) -> JoinHandle<()> {
             encoder: window.factory.create_command_buffer().into(),
         };
 
+        let assets = find_folder::Search::ParentsThenKids(1, 1)
+            .for_folder("assets")
+            .unwrap();
+        let mut glyphs = window
+            .load_font(assets.join("FiraSans-Regular.ttf"))
+            .unwrap();
+
         let initial_empty = ImageBuffer::new(width, height);
 
         let mut texture: G2dTexture = Texture::from_image(
@@ -130,6 +139,8 @@ fn run_window_thread(image_data: Arc<Canvas<Vec3<f64>>>) -> JoinHandle<()> {
 
         while let Some(event) = window.next() {
             if let Some(_) = event.render_args() {
+                let complete_percentage: String =
+                    format!("{:.0}%", image_data.get_complete() * 100.0);
                 let pixels: Vec<Vec3<f64>> = image_data.deref().into();
                 let max_intensity = max_intensity_from(&pixels);
                 let mut sub_pixels: Vec<u8> = pixels
@@ -153,6 +164,18 @@ fn run_window_thread(image_data: Arc<Canvas<Vec3<f64>>>) -> JoinHandle<()> {
                     texture_context.encoder.flush(device);
                     clear([1.0; 4], graphics);
                     image(&texture, context.transform, graphics);
+
+                    let text_position = context.transform.trans(10.0, 30.0);
+                    text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32)
+                        .draw(
+                            &complete_percentage,
+                            &mut glyphs,
+                            &context.draw_state,
+                            text_position,
+                            graphics,
+                        )
+                        .unwrap();
+                    glyphs.factory.encoder.flush(device);
                 });
             }
         }
@@ -190,8 +213,8 @@ fn f64_to_u8(value: f64) -> u8 {
     (value * 255.0) as u8
 }
 
-fn draw_on_canvas(
-    canvas: &Canvas<Vec3<f64>>,
+fn draw_to_image_data(
+    image_data: &ImageData<Vec3<f64>>,
     camera: &Camera<f64>,
     world: &Box<Hittable<f64>>,
     samples: i32,
@@ -202,25 +225,43 @@ fn draw_on_canvas(
     };
 
     for sample in 0..samples {
-        for j in (0..canvas.y_size()).rev() {
-            for i in 0..canvas.x_size() {
+        for j in (0..image_data.y_size()).rev() {
+            for i in 0..image_data.x_size() {
                 if i % 2 == j % 2 {
-                    draw_on_canvas_for_pixel(canvas, camera, world, i, j, pixel_location, sample);
+                    draw_to_image_data_for_pixel(
+                        image_data,
+                        camera,
+                        world,
+                        i,
+                        j,
+                        pixel_location,
+                        sample,
+                    );
                 }
             }
         }
-        for j in (0..canvas.y_size()).rev() {
-            for i in 0..canvas.x_size() {
+        image_data.update_complete(|_| (f64::from(sample) + 0.5) / f64::from(samples));
+        for j in (0..image_data.y_size()).rev() {
+            for i in 0..image_data.x_size() {
                 if i % 2 != j % 2 {
-                    draw_on_canvas_for_pixel(canvas, camera, world, i, j, pixel_location, sample);
+                    draw_to_image_data_for_pixel(
+                        image_data,
+                        camera,
+                        world,
+                        i,
+                        j,
+                        pixel_location,
+                        sample,
+                    );
                 }
             }
         }
+        image_data.update_complete(|_| (f64::from(sample) + 1.0) / f64::from(samples));
     }
 }
 
-fn draw_on_canvas_for_pixel(
-    canvas: &Canvas<Vec3<f64>>,
+fn draw_to_image_data_for_pixel(
+    image_data: &ImageData<Vec3<f64>>,
     camera: &Camera<f64>,
     world: &Box<Hittable<f64>>,
     i: usize,
@@ -229,13 +270,13 @@ fn draw_on_canvas_for_pixel(
     sample: i32,
 ) {
     let ray = camera.get_ray(
-        pixel_location(i, canvas.x_size()),
-        pixel_location(j, canvas.y_size()),
+        pixel_location(i, image_data.x_size()),
+        pixel_location(j, image_data.y_size()),
     );
     let pixel_colour = color(ray, &world, 0.0001, std::f64::MAX, 50);
     match sample {
-        0 => canvas.update_pixel(i, j, |_| pixel_colour),
-        _ => canvas.update_pixel(i, j, |old_avg| {
+        0 => image_data.update_pixel(i, j, |_| pixel_colour),
+        _ => image_data.update_pixel(i, j, |old_avg| {
             let old_sum = old_avg * f64::from(sample - 1);
             let new_sum = old_sum + pixel_colour;
             new_sum / f64::from(sample)
@@ -277,7 +318,7 @@ fn create_world() -> Box<Hittable<f64>> {
     world
 }
 
-fn output_ppm(image_data: Arc<Canvas<Vec3<f64>>>) -> () {
+fn output_ppm(image_data: Arc<ImageData<Vec3<f64>>>) -> () {
     println!("P3");
     println!("{} {}", image_data.x_size(), image_data.y_size());
     println!("255");
